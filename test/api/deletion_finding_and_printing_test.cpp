7c0e2a4ecc36a39470e3beada1e766c67f92291a
Simon Sasse <simonsasse97@andorra.imp.fu-berlin.de>
2020-11-23 13:22:06 +0100

    [TEST] CLI and API tests for output option of find_deletions.

--- test/api/deletion_finding_and_printing_test.cpp
+++ test/api/deletion_finding_and_printing_test.cpp
@@ -1,18 +1,26 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <fstream>
+#include <sstream>
+
 #include "find_deletions/deletion_finding_and_printing.hpp"
 
 TEST(group1, test_std_out)
-{
-    std::string expected{"##fileformat=VCFv4.2\n"
-                         "##source=iGenVarCaller\n"
-                         "CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO\n"
-                         "chr21	9435236	.	N	<DEL>	60	PASS	SVTYPE=DEL;SVLEN=-50;END=9435286\n"
-                         "chr21	11104574	.	N	<DEL>	60	PASS	SVTYPE=DEL;SVLEN=-248;END=11104822\n"};
-
-    testing::internal::CaptureStdout();
-    find_and_print_deletions(DATADIR"detect_breakends_shorted.vcf");
-    std::string std_cout = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(expected, std_cout);
+{   
+    find_and_print_deletions(DATADIR"detect_breakends_shorted.vcf", DATADIR"find_deletions_file_out_test.vcf");
+    std::ifstream f;
+    f.open(DATADIR"find_deletions_file_out_test.vcf");
+    std::stringstream buffer;
+    buffer << f.rdbuf();
+    std::string expected
+    {
+        "##fileformat=VCFv4.2\n"
+        "##source=iGenVarCaller\n"
+        "CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n"
+        "chr21\t9435236\t.\tN\t<DEL>\t60\tPASS\tSVTYPE=DEL;SVLEN=-50;END=9435286\n"
+        "chr21\t11104574\t.\tN\t<DEL>\t60\tPASS\tSVTYPE=DEL;SVLEN=-248;END=11104822\n"
+    };
+    EXPECT_TRUE(f.is_open());
+    EXPECT_EQ(buffer.str(), expected);
 }

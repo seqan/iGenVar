debdcb66b076b86ab83e5a130e9f549d0f62485e
Simon Sasse <simonsasse97@andorra.imp.fu-berlin.de>
2020-11-23 13:21:41 +0100

    [FEATURE, TEST] Making find_deletions write to an output file and not to std::out. And adding tests.

--- include/find_deletions/deletion_finding_and_printing.hpp
+++ include/find_deletions/deletion_finding_and_printing.hpp
@@ -2,7 +2,6 @@
 
 #include <seqan3/std/filesystem>
 #include <fstream>
-#include <iostream>
 #include <vector>
 
 #include "junction.hpp"
@@ -10,7 +9,8 @@
 /*! \brief Detects deletions out of the junction file.
  *
  * \param junction_file_path input junction file
+ * \param output_file_path output vcf file
  *
  * \details Extracts deletions out of given breakends / junctions.
  */
-void find_and_print_deletions(const std::filesystem::path & junction_file_path);
+void find_and_print_deletions(std::filesystem::path const & junction_file_path, std::filesystem::path const & output_file_path);

e3954b7d5a8f2cf444cbc517df6aaf86c5f9c573
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2021-03-06 17:32:32 +0100

    [FEATURE] Handle long reads in a separate function (detect_junctions_in_long_reads_sam_file), rename general function detect_junctions_in_alignment_file -> detect_structural_variations. [DOC] Update documentation
    Signed-off-by: Lydia Buntrock <lydia.buntrock@fu-berlin.de>

--- include/modules/clustering/simple_clustering_method.hpp
+++ include/modules/clustering/simple_clustering_method.hpp
@@ -4,7 +4,7 @@
 
 /*! \brief This method clusters junctions by merging identical junctions into a single cluster object.
  *
- * \param junctions - A vector of junctions
- * \param clusters - A vector of clusters
+ * \param junctions - a vector of junctions
+ * \param clusters - a vector of clusters
  */
 void simple_clustering_method(std::vector<Junction> & junctions, std::vector<Cluster> & clusters);
--- include/variant_detection/variant_detection.hpp
+++ include/variant_detection/variant_detection.hpp
@@ -4,13 +4,46 @@
 #include <vector>
 
 #include "method_enums.hpp"         // for enum clustering_methods and refinement_methods
+#include "structures/junction.hpp"  // for class Junction
+
+/*! \brief Detects junctions between distant genomic positions by analyzing a long read alignment file (sam/bam). The
+ *         detected junctions are stored in a vector.
+ *
+ * \param junctions - a vector of junctions
+ * \param alignment_long_reads_file_path - long reads input file, path to the sam/bam file
+ * \param insertion_file_path - output file path for the fasta file
+ * \param methods - list of methods for detecting junctions (0: cigar_string,
+ *                                                           1: split_read,
+ *                                                           2: read_pairs,
+ *                                                           3: read_depth)
+ * \param clustering_method - method for clustering junctions (0: simple_clustering
+ *                                                             1: hierarchical_clustering,
+ *                                                             2: self-balancing_binary_tree,
+ *                                                             3: candidate_selection_based_on_voting)
+ * \param refinement_method - method for refining breakends (0: no_refinement,
+ *                                                           1: sViper_refinement_method,
+ *                                                           2: sVirl_refinement_method)
+ * \param min_var_length - minimum length of variants to detect (default 30 bp)
+ *
+ * \details Detects junctions from the CIGAR strings and supplementary alignment tags of read alignment records.
+ *          We filter unmapped alignments, secondary alignments, duplicates and alignments with low mapping quality.
+ *          Then, the CIGAR string of all remaining alignments is analyzed.
+ *          For primary alignments, also the split read information is analyzed.
+ */
+void detect_junctions_in_long_reads_sam_file(std::vector<Junction> & junctions,
+                                             const std::filesystem::path & alignment_long_reads_file_path,
+                                             const std::filesystem::path & insertion_file_path,
+                                             const std::vector<detection_methods> methods,
+                                             const clustering_methods clustering_method,
+                                             const refinement_methods refinement_method,
+                                             const uint64_t min_var_length);
 
 /*! \brief Detects genomic variants by analyzing an alignment file (sam/bam). The detected
  *         variants are printed to a given file or stdout and insertion alleles are stored in a fasta file.
  *
  * \param alignment_short_reads_file_path - short reads input file, path to the sam/bam file
  * \param alignment_long_reads_file_path - long reads input file, path to the sam/bam file
- * \param insertion_file_path output file - path for the fasta file
+ * \param insertion_file_path - output file path for the fasta file
  * \param methods - list of methods for detecting junctions (1: cigar_string,
  *                                                           2: split_read,
  *                                                           3: read_pairs,

7713d8be3aa11ec3be883273d212ef10387af5b6
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2021-10-25 17:09:59 +0200

    [FEATURE] detect tandem duplications via tandem_dup_count

--- src/modules/sv_detection_methods/analyze_cigar_method.cpp
+++ src/modules/sv_detection_methods/analyze_cigar_method.cpp
@@ -27,30 +27,54 @@ void analyze_cigar(std::string const & read_name,
         using seqan3::get;
         int32_t length = get<0>(pair);
         seqan3::cigar::operation operation = get<1>(pair);
-        size_t tandem_dup_count = 0;
         if (operation == 'M'_cigar_operation || operation == '='_cigar_operation || operation == 'X'_cigar_operation)
         {
             pos_ref += length;
             pos_read += length;
         }
+        // -------------------------------------
         else if (operation == 'I'_cigar_operation) // I: Insertion (gap in the reference sequence)
         {
             if (length >= min_length)
             {
                 // Insertions cause one junction from the insertion location to the next base
                 auto inserted_bases = query_sequence | seqan3::views::slice(pos_read, pos_read + length);
-                Junction new_junction{Breakend{chromosome, pos_ref - 1, strand::forward},
-                                      Breakend{chromosome, pos_ref, strand::forward},
-                                      inserted_bases,
-                                      tandem_dup_count,
-                                      read_name};
-                if (gVerbose)
-                    seqan3::debug_stream << "INS: " << new_junction << "\n";
-                junctions.push_back(std::move(new_junction));
+        // ---------------- DUP ----------------
+                // Case - Duplication: The inserted bases include one or more copies of a duplicated sequence, with an
+                //                     origin somewhere else. -> global alignment
+                // ##ALT=<ID=DUP,Description="Duplication">
+                // TODO (23.7.21, irallia): This is Part of the Epic #144. Do we need a reference sequence for this?
+        // ---------------- DUP:TANDEM ----------------
+                size_t tandem_dup_count = 0;
+                int32_t pos_start_dup_seq{};
+                int32_t pos_end_dup_seq{};
+                // duplicated_bases = detect_tandem_duplication()
+                if (tandem_dup_count != 0)
+                {
+                    Junction new_junction{Breakend{chromosome, pos_start_dup_seq, strand::forward},
+                                          Breakend{chromosome, pos_end_dup_seq, strand::forward},
+                                          inserted_bases, // duplicated_bases,
+                                          tandem_dup_count,
+                                          read_name};
+                    if (gVerbose)
+                        seqan3::debug_stream << "DUP:TANDEM: " << new_junction << "\n";
+                    junctions.push_back(std::move(new_junction));
+                } else {
+        // ---------------- INS ----------------
+                    Junction new_junction{Breakend{chromosome, pos_ref - 1, strand::forward},
+                                          Breakend{chromosome, pos_ref, strand::forward},
+                                          inserted_bases,
+                                          0,
+                                          read_name};
+                    if (gVerbose)
+                        seqan3::debug_stream << "INS: " << new_junction << "\n";
+                    junctions.push_back(std::move(new_junction));
+                }
             }
             pos_read += length;
         }
-        else if (operation == 'D'_cigar_operation)
+        // ---------------- DEL ----------------
+        else if (operation == 'D'_cigar_operation) // D: Deletion (gap in the read)
         {
             if (length >= min_length)
             {
@@ -58,7 +82,7 @@ void analyze_cigar(std::string const & read_name,
                 Junction new_junction{Breakend{chromosome, pos_ref - 1, strand::forward},
                                       Breakend{chromosome, pos_ref + length, strand::forward},
                                       ""_dna5,
-                                      tandem_dup_count,
+                                      0,
                                       read_name};
                 if (gVerbose)
                     seqan3::debug_stream << "DEL: " << new_junction << "\n";

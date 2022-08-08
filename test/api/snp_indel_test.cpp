63d973379061b51a69e7238e8bef7be317eef7c3
Jörg Winkler <j.winkler@fu-berlin.de>
2022-08-08 14:11:46 +0200

    write and adapt tests

--- test/api/snp_indel_test.cpp
+++ test/api/snp_indel_test.cpp
@@ -1,17 +1,18 @@
 #include <gtest/gtest.h>
 
+#include <fstream>
 #include <vector>
 
 #include "variant_detection/snp_indel_detection.hpp"
 
-TEST(activity_analysis, input_empty)
+TEST(activity_detection, input_empty)
 {
     Activity activity{};
     auto regions = get_active_regions(activity);
     EXPECT_TRUE(regions.empty());
 }
 
-TEST(activity_analysis, input_zero)
+TEST(activity_detection, input_zero)
 {
     Activity activity{};
     activity.values.emplace_back(150, 0U); // 150x 0
@@ -20,7 +21,7 @@ TEST(activity_analysis, input_zero)
     EXPECT_TRUE(regions.front().empty());
 }
 
-TEST(activity_analysis, input_active_everywhere)
+TEST(activity_detection, input_active_everywhere)
 {
     Activity activity{};
     activity.values.emplace_back(150, 9999U); // 150x 9999
@@ -30,7 +31,7 @@ TEST(activity_analysis, input_active_everywhere)
     EXPECT_EQ(regions.front().front(), (std::pair<int, int>{0, 150}));
 }
 
-TEST(activity_analysis, input_interval)
+TEST(activity_detection, input_interval)
 {
     Activity activity{};
     activity.values.emplace_back(150, 0U);
@@ -43,4 +44,69 @@ TEST(activity_analysis, input_interval)
     EXPECT_EQ(regions.front().size(), 2U);
     EXPECT_EQ(regions.front()[0], (std::pair<int, int>{30, 50}));
     EXPECT_EQ(regions.front()[1], (std::pair<int, int>{131, 149}));
+}
+
+TEST(activity_analysis, incompatible_genome)
+{
+    Genome genome;
+    std::filesystem::path const reads = DATADIR"simulated.minimap2.hg19.coordsorted_cutoff.sam";
+    // genome has too few sequences (0 instead of 1)
+    EXPECT_THROW(analyze_activity(reads, genome, 20UL), std::runtime_error);
+
+    using namespace seqan3::literals;
+    genome.names.emplace_back("chr8");
+    genome.seqs.push_back("ACG"_dna5);
+    // genome length mismatch (3 instead of 46709983)
+    EXPECT_THROW(analyze_activity(reads, genome, 20UL), std::runtime_error);
+}
+#include <seqan3/core/debug_stream.hpp>
+TEST(activity_analysis, skip_ref)
+{
+    // Create a SAM file with three references.
+    std::filesystem::path reads{std::filesystem::temp_directory_path()/"reads.sam"};
+    {
+        std::ofstream samfile(reads);
+        samfile << "@HD\tVN:1.6\tSO:coordinate\n"
+                << "@SQ\tSN:chr1\tLN:4\n"
+                << "@SQ\tSN:chr2\tLN:3\n"
+                << "test1\t16\tchr2\t1\t60\t10M\t=\t1\t0\tG\tF\n"; // chr1 skipped
+        samfile.close();
+    }
+
+    Genome genome;
+    using namespace seqan3::literals;
+    genome.names.emplace_back("chr1");
+    genome.seqs.push_back("ACGT"_dna5);
+    genome.names.emplace_back("chr2");
+    genome.seqs.push_back("ACG"_dna5);
+
+    auto act = analyze_activity(reads, genome, 20UL);
+    EXPECT_EQ(act.refmap, (std::vector<int>{-1, 1})); // first unused, second maps to seq 1
+    EXPECT_TRUE(act.values[0].empty()); // first is unused => empty
+    EXPECT_EQ(act.values[1], (std::vector<unsigned>{0, 0, 0})); // second has length 3 (=ref len), no activity added
+}
+
+TEST(store_snp, same_junction)
+{
+    using namespace seqan3::literals;
+    std::set<Junction> junctions;
+    seqan3::dna5_vector bufR = "ACG"_dna5;
+    seqan3::dna5_vector bufH = "CT"_dna5;
+    int pos = 30;
+    std::string const ref_name = "chr1";
+
+    store_snp(junctions, bufR, bufH, pos, ref_name, 0.1);
+    bufR = "ACG"_dna5;
+    bufH = "CG"_dna5;
+    store_snp(junctions, bufR, bufH, pos, ref_name, 0.6); // this junction compares smaller
+    bufR = "ACG"_dna5;
+    bufH = "CT"_dna5;
+    store_snp(junctions, bufR, bufH, pos, ref_name, 0.3); // add the initial junction again
+
+    EXPECT_EQ(junctions.size(), 2UL);
+    auto jctIt = junctions.begin(); // check 1st junction
+    EXPECT_FLOAT_EQ(jctIt->get_quality(), 0.6F);
+    ++jctIt; // go to 2nd junction
+    EXPECT_FLOAT_EQ(jctIt->get_quality(), 0.4F); // 0.1 + 0.3
+    EXPECT_TRUE(*jctIt != *junctions.begin());
 }
--- test/data/datasources.cmake
+++ test/data/datasources.cmake
@@ -19,29 +19,29 @@ declare_datasource (FILE mini_example_reference.fasta
 # copies file to <build>/data/paired_end_mini_example.sam
 declare_datasource (FILE paired_end_mini_example.sam
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example/paired_end_mini_example.sam
-                    URL_HASH SHA256=9dc47068c9a685ae414d9d943b512ab14d1d8de041d805e61c7f8352831a51b3)
+                    URL_HASH SHA256=58961c8f016dbcaa6d38806f9cd3e90dbacda4f40af4693db9540c5544b9367a)
 
 # copies file to <build>/data/single_end_mini_example.sam
 declare_datasource (FILE single_end_mini_example.sam
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example/single_end_mini_example.sam
-                    URL_HASH SHA256=5dbf1d7f41b392bd34ff765b65ac9be73b08246aad774a2399a83523ca45cf41)
+                    URL_HASH SHA256=b7ad2c43c444e4897e883c01f8efe3bbdd2b185d1c30efaa16be28ca2b04049d)
 
 # copies file to <build>/data/output_err.txt
 declare_datasource (FILE output_err.txt
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example/output_err.txt
-                    URL_HASH SHA256=f44b6522c6df97506a70e6cc961c4d2b1caf2cef3b245688d327ce22c7425133)
+                    URL_HASH SHA256=d30107b5222a0768db9cbe6f4f7c4b11b7c530057f99c969d891a56f596f6320)
 
 # copies file to <build>/data/output_res.vcf
 declare_datasource (FILE output_res.vcf
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example/output_res.vcf
-                    URL_HASH SHA256=9b70052748679b2e29d4985ad9864380c6f7f66f56c6797f73421dd2598a1455)
+                    URL_HASH SHA256=938d73ef495f4cddccc10eaefbda7c3ec934926798edf5a8db4d1b00ae10e646)
 
 # copies file to <build>/data/output_short_and_long_err.txt
 declare_datasource (FILE output_short_and_long_err.txt
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example/output_short_and_long_err.txt
-                    URL_HASH SHA256=568ed8a913fc05f31f91051df6ef17be3b55c0afd79c5f6c657ad6342f897fd2)
+                    URL_HASH SHA256=86dfe165ea871101b52c37dbcb6db8b4ccf86a2484d12277ecdd4b5e75be5cc8)
 
 # copies file to <build>/data/output_short_and_long_res.vcf
 declare_datasource (FILE output_short_and_long_res.vcf
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example/output_short_and_long_res.vcf
-                    URL_HASH SHA256=71b92fb734be1fa5b4d5973207920c17e8a2d02d9351a413fb401401eb500ad4)
+                    URL_HASH SHA256=a95fd7e6f882ea977feaef541ec8fc92906b5fda329088433670fcaa05d36a50)
